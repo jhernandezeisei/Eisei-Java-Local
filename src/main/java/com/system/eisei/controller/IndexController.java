c8cafdcd5aceee5fdfc0e7dc927ca814e5dd60e9
Jose Hernandez <jesus.hernandez@eisei.net.mx>
2022-06-27 10:31:15 -0500

    Configuracion a bd
    Se agregan archivos para configuración a base de datos y consulta a SP

--- src/main/java/com/system/eisei/controller/IndexController.java
+++ src/main/java/com/system/eisei/controller/IndexController.java
@@ -1,48 +1,45 @@
 package com.system.eisei.controller;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import javax.sql.DataSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import com.system.eisei.DaoImpl.LoginDaoImpl;
+import com.system.eisei.dao.LoginDao;
+import com.system.eisei.model.Usuario;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-@Controller
-public class IndexController {
-    
-    @RequestMapping(value="/login", method = RequestMethod.GET)
-    public ModelAndView index(){
-        ModelAndView indexModel = new ModelAndView("index", "command", new Object());
-        
-//        try {
-//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-//            String connectionUrl =
-//                    "jdbc:sqlserver://EDGO-026\\SQLEXPRESS;"
-//                            + "database=JAVALOCALEISEI;"
-//                            + "user=sa;"
-//                            + "password=Eiseidgo2022;";
-//        } catch (Exception e) {
-//            
-//        }
-        
-//        String connectionUrl = "jdbc:sqlserver://EDGO-026\\SQLEXPRESS;"
-//                                    + "database=JAVALOCALEISEI;"
-//                                    + "user=sa;"
-//                                    + "password=Eiseidgo2022;";
-//        try (Connection connection = DriverManager.getConnection(connectionUrl);) {
-//            // Code here.
-//            System.out.println("Conexión a BD exitosa.");
-//            
-//        }
-//        // Handle any errors that may have occurred.
-//        catch (SQLException e) {
-//            System.out.println(e);
-//            e.printStackTrace();
-//        }
-        
-        return indexModel;
+public class IndexController extends HttpServlet {
+    Usuario user = new Usuario();
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        doPost(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String usuario = request.getParameter("usuario");
+        String contraseña = request.getParameter("contrasena");
+        user.setUsuario(usuario);
+        user.setContraseña(contraseña);
+        LoginDao ld = new LoginDaoImpl();
+        Usuario u = ld.login(user);
+        if(u.getUsuario()!=null){
+            request.getRequestDispatcher("welcome.jsp").forward(request, response);
+        }else{
+            response.sendRedirect("bienvenida.jsp");
+        }
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
     }
 }
