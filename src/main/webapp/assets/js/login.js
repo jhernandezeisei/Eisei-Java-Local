72a73e70127217ef522c048750e481d846139c74
jhernandezeisei <89152740+jhernandezeisei@users.noreply.github.com>
2022-07-04 17:24:47 -0500

    Revert "Branch/jose gh"

--- src/main/webapp/assets/js/login.js
+++ src/main/webapp/assets/js/login.js
@@ -4,44 +4,18 @@ var contraseña = '';
 function buscar() {
     usuario = $('#txtusuario').val();
     contraseña = $('#txtpass').val();
-    
     if (usuario === "") {
         alert("Complete el campo de usuario");
     } else if (contraseña === "") {
         alert("Complete el campo de contraseña");
     } else {
-        sendRequestPOSTloggin({Usuario: usuario, Contrasena: contraseña}, "../SystemEiseiApp/Userlogin", usuario);
-        
+        validar();
     }
-}
-
-function sendRequestPOSTloggin(fobj, actionURL, user) {
-    $.ajax({
-        type: "POST",
-        url: actionURL,
-        data: fobj,
-        async: true,
-        success: function (data) {
-            console.log(fobj);
-            if (data == 'usuario') {
-                    window.open("welcome.jsp", "_self");
-                } else {
-                alert("Credenciales incorrectas")
-                }
-        },
-        error: function (jqxhr, settings, thrownError) {
-            console.log("jqxhr.responseText=" + jqxhr.responseText);
-        },
-        fail: function (jqXHR, textStatus) {
-            console.log(">>sendRequestJQ(): thrownError:" + thrownError);
-            console.log(">>sendRequestJQ(): jqXHR:" + jqXHR);
-        }
-    });
 }
 
 function validar() {
     if (usuario === "root" && contraseña === "123") {
-        window.open("welcome.jsp", "_self");
+        window.open("prueba.jsp", "_self");
     } else {
         alert("Usuario incorrecto");
     }
@@ -53,8 +27,6 @@ function showPopupMessageGeneric(tipo, menssage) {
     $('#tipo').text(tipo);
     $('#mensajeError').text(menssage);
 }
-
-
 
 
 
